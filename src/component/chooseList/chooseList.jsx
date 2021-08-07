9387a4be6288f9d95790ae72603d14b368bb4b31
Creniaut Benjamin <creniaut.benjamin@gmail.com>
2021-08-07 02:25:42 +0200

    bug listes

--- src/component/chooseList/chooseList.jsx
+++ src/component/chooseList/chooseList.jsx
@@ -39,16 +39,16 @@ function ChooseList({number}) {
 
     return (
         <div>
-            <button className="button" key={1} onClick={(e) => selectLevel(number, 5,e)}>Liste 1</button>
-            <button className="button" key={2} onClick={(e) => selectLevel(number, 6,e)}>Liste 2</button>
-            <button className="button" key={3} onClick={(e) => selectLevel(number, 7,e)}>Liste 3</button>
-            <button className="button" key={4} onClick={(e) => selectLevel(number, 5,e)}>Liste 4</button>
+            <button className="button" key={1} onClick={(e) => selectLevel(number, 1,e)}>Liste 1</button>
+            <button className="button" key={2} onClick={(e) => selectLevel(number, 2,e)}>Liste 2</button>
+            <button className="button" key={3} onClick={(e) => selectLevel(number, 3,e)}>Liste 3</button>
+            <button className="button" key={4} onClick={(e) => selectLevel(number, 4,e)}>Liste 4</button>
             <button className="button" key={5} onClick={(e) => selectLevel(number, 5,e)}>Liste 5</button>
-            <button className="button" key={6} onClick={(e) => selectLevel(number, 5,e)}>Liste 6</button>
-            <button className="button" key={7} onClick={(e) => selectLevel(number, 5,e)}>Liste 7</button>
-            <button className="button" key={8} onClick={(e) => selectLevel(number, 5,e)}>Liste 8</button>
-            <button className="button" key={9} onClick={(e) => selectLevel(number, 5,e)}>Liste 9</button>
-            <button className="button" key={10} onClick={(e) => selectLevel(number, 5,e)}>Liste 10</button>
+            <button className="button" key={6} onClick={(e) => selectLevel(number, 6,e)}>Liste 6</button>
+            <button className="button" key={7} onClick={(e) => selectLevel(number, 7,e)}>Liste 7</button>
+            <button className="button" key={8} onClick={(e) => selectLevel(number, 8,e)}>Liste 8</button>
+            <button className="button" key={9} onClick={(e) => selectLevel(number, 9,e)}>Liste 9</button>
+            <button className="button" key={10} onClick={(e) => selectLevel(number, 10,e)}>Liste 10</button>
         </div>
         )      
         
